512207bd311dc99613ac756df12950b2e7c155c3
danny tran <dannyhaitran@outlook.com>
2026-01-28 11:20:39 -0800

    Changed directory

---
+++ PUSH_TO_GITHUB.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Commands to push to GitHub
+# Run these in the homes4sale-replica directory
+
+git remote add origin https://github.com/dannytrann/craig-spikman-website.git
+git branch -M main
+git push -u origin main
